e34d5091cacbb192a231ff9c28797bc8e8f24d2b
AnastasiaLoveTea <145138564+AnastasiaLoveTea@users.noreply.github.com>
2025-12-11 22:44:13 +0200

    lab8

--- dlm/src/main/java/org/example/dlm/composite/DownloadComponent.java
+++ dlm/src/main/java/org/example/dlm/composite/DownloadComponent.java
@@ -4,14 +4,13 @@ import java.util.UUID;
 
 public interface DownloadComponent {
 
-    // «Operation()» з діаграми – наша основна дія
     void start();
     void pause();
     void cancel();
 
-    // допоміжні операції для відображення прогресу/статистики
     long getTotalBytes();
     long getReceivedBytes();
 
     String getName();
 }
+
